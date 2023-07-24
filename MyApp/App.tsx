6b7aa0048223471f6dc4f7b42fe3a9cebce714c0
Walter Lambardi <walterlambardi@MMBA-B569.local>
2023-07-24 17:22:23 -0300

    Add Fabric Native Component RTNCenteredText

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -7,6 +7,7 @@
 import React, { useState } from 'react';
 import { SafeAreaView, StatusBar, Text, Button } from 'react-native';
 import RTNCalculator from 'rtn-calculator/js/NativeCalculator';
+import RTNCenteredText from 'rtn-centered-text/js/RTNCenteredTextNativeComponent';
 
 const App: () => JSX.Element = () => {
   const [result, setResult] = useState<number | null>(null);
@@ -22,6 +23,10 @@ const App: () => JSX.Element = () => {
           const value = await RTNCalculator?.add(3, 7);
           setResult(value ?? null);
         }}
+      />
+      <RTNCenteredText
+        text="Hello World!"
+        style={{width: '100%', height: 30}}
       />
     </SafeAreaView>
   );
